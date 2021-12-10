d8804c1296a350bde3b74691296fea481f3264b4
Andrei Btc <andreibtc@gmail.com>
2021-12-10 14:43:57 +0000

    added functionality for navbar transparency base on scroll position

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 console.log("Connected!");
 
-// Navbar manipulation
+// Navbar manipulation - Modifies navbar transparency based on the scroll position
 const navbar = document.querySelector("#navbar");
+const navTransparency = () => {
+  scrollY === 0
+    ? (navbar.style.backgroundColor = "#fefefe00")
+    : scrollY === 25
+    ? (navbar.style.backgroundColor = "#ffffff3e")
+    : scrollY === 50
+    ? (navbar.style.backgroundColor = "#ffffff84")
+    : scrollY === 75
+    ? (navbar.style.backgroundColor = "#ffffffd0")
+    : (navbar.style.backgroundColor = "#ffffff");
+};
 
-document.addEventListener(
-  "scroll",
-  () => (navbar.style.backgroundColor = "#fff")
-);
+document.addEventListener("scroll", navTransparency);
 
 // Feature - Changing the background gradient as per user input
 const gradientStart = document.querySelector("#gradient-start");
